d7b077db487696b1e4f0047b3e0b759f9ca577dc
Flo-R1der <findigutt-github@yahoo.com>
2026-08-06 23:14:09 +0200

    add CONFIG_SCHEMA + to_code() / activate external component

--- components/system_status_led_fx/__init__.py
+++ components/system_status_led_fx/__init__.py
@@ -1,4 +1,11 @@
 # components/system_status_led_fx/__init__.py
 import esphome.codegen as cg
+import esphome.config_validation as cv
 
 CODEOWNERS = ["@Flo-R1der"]
+
+CONFIG_SCHEMA = cv.Schema({})
+
+async def to_code(config):
+    cg.add_global(cg.RawStatement('#include "esphome/components/system_status_led_fx/system_status_led_effect.h"'))
+    
